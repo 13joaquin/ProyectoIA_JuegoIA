d28f3144d0001e66a5e7761f6eb24c35c50a4e38
13joaquin <juaquin.david13@gmail.com>
2024-03-27 18:57:00 -0600

    Parte 1
    Parte 1 del Proyecto de TIC TAC TOE completa

--- tictictie.py
+++ tictictie.py
@@ -1,13 +1,37 @@
+#from asyncio import events
+import sys
+#from turtle import bgcolor, width
 import pygame, sys
-from pygame.locals import *
+
+# ----- PYGAME SETUP-----
 pygame.init()
+#---- ALTURA PYGAME-----
+WIDTH = 600
+HEIGHT = 600
+#----COLOR DEL FONDO----
+BG_COLOR = (28,170,250)
+#----OTRO COLOR-----
+RED = (255,0,0)
+LINE_COLOR = (23,145,135)
+LINE_WIDTH = 15
 
-DySPLAY = pygame.display.set_mode((400, 300))
-pygame.display.set_caption('Hello Wordl')
+screen = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption('Tic Tac Toe AI')
+screen.fill(BG_COLOR)
+#pygame.draw.line(screen,RED,(10,10),(300,300),10)
 
-while True: #main game loop
-    for evet in pygame.event.get():
-     if evet.type ==quit:
-       pygame.quit()
-       sys.exit()
-    pygame.displayx.update()
+def draw_line():
+    # 1 Horizontal
+    pygame.draw.line(screen, LINE_COLOR,(0,200),(600,200),LINE_WIDTH)
+    # 2 Horizontal
+    pygame.draw.line(screen, LINE_COLOR,(0,400),(600,400),LINE_WIDTH)
+    # 1 Vertical
+    pygame.draw.line(screen, LINE_COLOR,(200,0),(200,600),LINE_WIDTH)
+    # 2 Vertical
+    pygame.draw.line(screen, LINE_COLOR,(400,0),(400,600),LINE_WIDTH)
+draw_line()
+while True:
+    for event in pygame.event.get():
+     if event.type == pygame.QUIT:
+         sys.exit()
+    pygame.display.update()
