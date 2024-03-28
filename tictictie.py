6d4818f7959ef46ad7a155ed02493e0d234aa1cc
13joaquin <juaquin.david13@gmail.com>
2024-03-27 20:19:36 -0600

    Parte 2
    Parte 2 del Proyecto de TIC TAC TOE completa

--- tictictie.py
+++ tictictie.py
@@ -1,7 +1,9 @@
 #from asyncio import events
+from ast import If
 import sys
 #from turtle import bgcolor, width
 import pygame, sys
+import numpy as np 
 
 # ----- PYGAME SETUP-----
 pygame.init()
@@ -14,12 +16,18 @@ BG_COLOR = (28,170,250)
 RED = (255,0,0)
 LINE_COLOR = (23,145,135)
 LINE_WIDTH = 15
+BOARD_ROWS = 3
+BOARD_COLS =3
 
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption('Tic Tac Toe AI')
 screen.fill(BG_COLOR)
 #pygame.draw.line(screen,RED,(10,10),(300,300),10)
 
+#board
+board = np.zeros((BOARD_ROWS, BOARD_COLS))
+#print(board)
+#---Dibujar Lineas---
 def draw_line():
     # 1 Horizontal
     pygame.draw.line(screen, LINE_COLOR,(0,200),(600,200),LINE_WIDTH)
@@ -29,7 +37,29 @@ def draw_line():
     pygame.draw.line(screen, LINE_COLOR,(200,0),(200,600),LINE_WIDTH)
     # 2 Vertical
     pygame.draw.line(screen, LINE_COLOR,(400,0),(400,600),LINE_WIDTH)
+
+#-Esta disponible el cuadro del medio-
+def mark_square(row, col, player):
+    board[row][col] = player
+
+def available_square(row, col):
+    return board[row][col] == 0
+def is_board_full():
+    for row in range(BOARD_ROWS):
+     for col in range(BOARD_COLS):
+      if board[row][col] == 0:
+        return False
+    return True
+#false
+print(is_board_full())
+#marking all squares
+for row in range(BOARD_ROWS):
+     for col in range(BOARD_COLS):
+         mark_square(row,col,1)
+#board total Full -true
+print(is_board_full())
 draw_line()
+# ----MainLoop----
 while True:
     for event in pygame.event.get():
      if event.type == pygame.QUIT:
