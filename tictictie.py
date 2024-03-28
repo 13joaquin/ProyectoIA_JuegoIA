c279ede64737a870a90f9a5ab9fa12533e5ae42b
Edvin Perez <edvinperez237@gmail.com>
2024-03-28 12:29:43 -0600

    Parte 3 Parte 3 del Proyecto de TIC TAC TOE completa

--- tictictie.py
+++ tictictie.py
@@ -17,7 +17,14 @@ RED = (255,0,0)
 LINE_COLOR = (23,145,135)
 LINE_WIDTH = 15
 BOARD_ROWS = 3
-BOARD_COLS =3
+BOARD_COLS = 3
+CIRCLE_RADIUS = 60
+CIRCLE_WIDTH = 15
+CIRCLE_COLOR = (239, 231, 200)
+CROSS_WIDTH = 25
+CROSS_COLOR = (66, 66, 66)
+SPACE = 55
+
 
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption('Tic Tac Toe AI')
@@ -38,6 +45,16 @@ def draw_line():
     # 2 Vertical
     pygame.draw.line(screen, LINE_COLOR,(400,0),(400,600),LINE_WIDTH)
 
+def draw_figures():
+    for row in range(BOARD_ROWS):
+       for col in range(BOARD_COLS):
+          if board[row][col] == 1:
+             pygame.draw.circle(screen, CIRCLE_COLOR, (int(col * 200 + 100), int(row * 200 + 100)), CIRCLE_RADIUS, CIRCLE_WIDTH)
+          elif board[row][col] == 2:
+             pygame.draw.line(screen, CROSS_COLOR, (col * 200 + SPACE, row * 200 + 200 - SPACE), (col * 200 + 200 - SPACE, row * 200 + SPACE), CROSS_WIDTH)  
+             pygame.draw.line(screen, CROSS_COLOR, (col * 200 + SPACE, row * 200 + SPACE), (col * 200 + 200 - SPACE, row * 200 + 200 - SPACE), CROSS_WIDTH)
+          
+
 #-Esta disponible el cuadro del medio-
 def mark_square(row, col, player):
     board[row][col] = player
@@ -50,18 +67,31 @@ def is_board_full():
       if board[row][col] == 0:
         return False
     return True
-#false
-print(is_board_full())
-#marking all squares
-for row in range(BOARD_ROWS):
-     for col in range(BOARD_COLS):
-         mark_square(row,col,1)
-#board total Full -true
-print(is_board_full())
+
 draw_line()
+player = 1
 # ----MainLoop----
 while True:
     for event in pygame.event.get():
      if event.type == pygame.QUIT:
          sys.exit()
+
+     if event.type == pygame.MOUSEBUTTONDOWN:
+         mouseX = event.pos[0] #x
+         mouseY = event.pos[1] #y
+        
+         clicked_row = int(mouseY // 200)
+         clicked_col = int(mouseX // 200)
+
+         if available_square(clicked_row, clicked_col):
+             if player == 1:
+                mark_square(clicked_row, clicked_col, 1)
+                player = 2
+
+             elif player == 2:
+                mark_square(clicked_row, clicked_col, 2)
+                player = 1
+
+             draw_figures()
+             
     pygame.display.update()
